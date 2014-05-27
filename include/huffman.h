c1fb1fd44a5154bd23f573df473b10238fa5ac59
kurisuter <christelle.bodard@hotmail.com>
2014-05-27 16:40:15 +0200

    modifs dans gestion_fichier

---
+++ include/huffman.h
@@ -0,0 +1,39 @@
+/* 
+ * File:   huffman.h
+ * Author: kurisuter
+ *
+ * Created on 27 mai 2014, 11:17
+ */
+
+#ifndef HUFFMAN_H
+#define	HUFFMAN_H
+#include "arbre.h"
+
+typedef struct _listch liste_chainee;
+typedef struct _listch{
+	Arbre * arbre;
+	struct _listch *suivant;
+	
+}liste_chainee;
+
+/******************************************
+ * opérations sur le tableau de répétition*
+ * ****************************************/
+//renvoie l'indice du "symbole" répété le moins souvent
+int indice_minimum(int tab_repetition[]);
+//renvoie la somme des éléments du tableau
+int somme_element(int tab_repetition[]);
+
+liste_chainee * ajouterElement(liste_chainee *liste, Arbre * a);
+liste_chainee * supprimerElement(liste_chainee * liste, Arbre * arbre);
+liste_chainee * tab_to_list(int tab_repetition[]);
+//copie tab2 dans tab1
+void copier_tab(int tab1[],int tab2[]);
+//créé l'arbre de huffman à partir du tableau de répétition
+Arbre * creer_arbre_huffman(int tab_repetition[]);
+
+void afficher(liste_chainee * liste);
+//test du module huffman
+void test_huffman();
+#endif	/* HUFFMAN_H */
+
---
+++ source/huffman.c
@@ -0,0 +1,160 @@
+#include "huffman.h"
+
+//renvoie l'indice du "symbole" répété le moins souvent
+int indice_minimum(int tab_repetition[])
+{
+    int i;
+    int min=0;
+    for(i=0;i<256;i++)
+    {
+        if((tab_repetition[i] < tab_repetition[min]) && (tab_repetition[i]!= 0))
+        {
+            min = i;
+        }
+    }
+    return min; 
+}
+
+//renvoie la somme des éléments du tableau
+int somme_element(int tab_repetition[]){
+    int i;
+    int somme =0;
+    for(i=0;i<256;i++){
+        somme+=tab_repetition[i];
+    }
+    return somme;
+}
+
+liste_chainee * ajouterElement(liste_chainee *liste, Arbre * a){
+	liste_chainee * temp;
+	temp = (liste_chainee *)malloc(sizeof(liste_chainee));
+	temp->arbre = a;
+	temp->suivant = liste;
+	return temp;
+}
+
+liste_chainee * supprimerElement(liste_chainee * liste, Arbre * arbre){
+	liste_chainee * temp;
+	liste_chainee * elem_precedent;
+	int trouve = 0;
+	temp =liste;
+	elem_precedent= temp;
+	while((temp != NULL) && (trouve !=1) ){
+		if(temp->arbre == arbre){
+			trouve = 1;
+		}else{
+			elem_precedent = temp;
+			temp = temp->suivant;
+		}
+	}
+	if(trouve == 1){
+		if(elem_precedent == temp){
+			liste = temp->suivant;
+			
+		}else{
+			elem_precedent->suivant = temp->suivant;
+		}
+	}
+	return liste;
+}
+
+liste_chainee * tab_to_list(int tab_repetition[])
+{
+    int i;
+    liste_chainee *liste =NULL;
+    Arbre * a;
+    for(i=0;i<256;i++)
+    {
+        if(tab_repetition[i] !=0){
+            a = ajouter_nouveau_parent(NULL,NULL,0,tab_repetition[i]);
+            printf("proba = %d ",a->proba );
+            liste =ajouterElement(liste,a);
+	}
+    }
+    return liste;
+            
+}
+
+//
+Arbre * recherche_mini(liste_chainee *liste){
+	liste_chainee * temp;
+	Arbre * arbre;
+	temp=liste;
+	arbre = temp->arbre;
+	temp = temp->suivant;
+	while(temp != NULL){
+		if(temp->arbre->proba < arbre->proba){
+			arbre = temp->arbre;
+		}
+		temp = temp->suivant;
+	}
+	return arbre;
+}
+
+//créé l'arbre de huffman à partir du tableau de répétition
+Arbre * creer_arbre_huffman(int tab_repetition[])
+{
+    Arbre * a;
+    Arbre * a1;
+    Arbre * a2;
+    liste_chainee *liste;
+    int taille_fichier = somme_element(tab_repetition);
+    
+    liste = tab_to_list(tab_repetition);
+    
+    while(liste->arbre->proba != taille_fichier){
+        afficher(liste);
+        a1 = recherche_mini(liste);
+        liste = supprimerElement(liste, a1);
+        a2 = recherche_mini(liste);
+        liste = supprimerElement(liste, a2);
+        afficher(liste);
+        a = ajouter_nouveau_parent(a1,a2,0,((a1->proba) +(a2->proba)));
+        printf("proba : a1 %d  a2 %d  a %d \n",a1->proba,a2->proba,a->proba);
+        liste= ajouterElement(liste, a);
+        afficher(liste);
+    }
+    
+    return liste->arbre;
+}
+/*
+void remplir_tab_longueur(Arbre * a)
+{
+    int i;
+    if((a->fils_gauche ==NULL ) && (a->fils_droit ==NULL ))
+    {
+        
+    }else
+    {
+        
+    }
+}*/
+
+void afficher(liste_chainee * liste){
+	liste_chainee *temp;
+	temp = liste;
+	while(temp != NULL){
+		if(temp->arbre !=NULL){
+			printf(" %d ", temp->arbre->proba);
+		}else{
+			printf("\nELEMENT VIDE");		
+		}
+		temp = temp->suivant;
+	}
+	printf("\n");
+}
+
+//test du module huffman
+void test_huffman()
+{
+    int tab[256] = {0};
+    Arbre * a;
+    tab[5]= 2;
+    tab[4]= 3;
+    tab[19] =4;
+    tab[22] =4;
+    tab[11] =6;
+    
+    a = creer_arbre_huffman(tab);
+    affiche_arbre(a);
+}
